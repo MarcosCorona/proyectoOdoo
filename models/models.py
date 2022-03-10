6707a4a53bbfd1c633556f583532fd0ca75f2b81
Marcos <Marcoscoronadionisio@gmail.com>
2022-03-10 15:22:28 +0100

    Creación de modelos para el proyecto, modulo1

--- models/models.py
+++ models/models.py
@@ -2,5 +2,26 @@ from odoo import models, fields, api
 
 class tienda(models.Model):
     _nombre = 'tiendas.tienda'
-    _descripcion = 'Define los datos de cada tienda.'
+    _descripcion = 'Define los datos de cada tienda.' 
+
+    #atributos
+    idTienda = fields.Integer(string='Id ', required=True)
+    nombreTienda = fields.Char(string='Nombre tienda ',required=True)
+    direccionTienda = fields.Char(string='Direccion tienda ',required=True)
+
+class trabajador(models.Model):
+    _name = 'tiendas.trabajador'
+    _descripcion = 'Atributos de trabajador por tienda.'
+
+    #Atributos
+    
+    dniTrabajador = fields.Char(string='DNI',required=True)
+    nombreTrabajador = fields.Char(string='Nombre',required=True)
+    correoTrabajador = fields.Char(string='Correo',required=True)
+    fechaNacimiento = fields.Date(string='Fecha nacimiento', required=True, default = fields.date.today())
+
+
+
+
+
     
