f1e116cfbe0baf00d4b17b3355b773457c396b06
Marcos <Marcoscoronadionisio@gmail.com>
2022-03-11 19:10:42 +0100

    asd

--- __manifest__.py
+++ __manifest__.py
@@ -23,7 +23,7 @@
 
     # always loaded
     'data': [
-        'security/tiendas_security.xml'
+        'security/tiendas_security.xml',
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
